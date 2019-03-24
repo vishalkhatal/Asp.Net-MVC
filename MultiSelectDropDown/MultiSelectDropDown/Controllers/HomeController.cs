b54c1ea25171ae95d89d77292185cd96d3eab3a2
Vishal Khatal <vishal.khatal2206@gmail.com>
2019-03-24 10:31:43 +0530

    Showing multiselect dropdown in MVC application

---
+++ MultiSelectDropDown/MultiSelectDropDown/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MultiSelectDropDown.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.WorkingDays = GetWorkingDayValues();
+            return View();
+        }
+        private List<SelectListItem> GetWorkingDayValues()
+        {
+            return Enum.GetValues(typeof(DayOfWeek)).Cast<DayOfWeek>().Select(v => new SelectListItem
+            {
+                Text = v.ToString(),
+                Value = ((int)v).ToString()
+            }).ToList();
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
